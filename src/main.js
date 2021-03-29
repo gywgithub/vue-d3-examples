6ec21c0dc679f5fe40ebfd39fc78a14d1f534c33
gywgithub <qingyi_w@outlook.com>
2021-03-29 18:09:47 +0800

    perf(packgage.json): clear dependencies

--- src/main.js
+++ src/main.js
@@ -3,23 +3,9 @@ import App from './App.vue'
 import router from './router'
 import { vuetify, i18n } from '@/plugins/vuetify'
 import VueParticles from 'vue-particles'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 Vue.use(VueParticles)
-
-require('./mock.js')
-
-axios.defaults.withCredentials = true
-
-axios.interceptors.response.use(function (response) {
-  return response
-}, function (error) {
-  return Promise.reject(error)
-})
-
-Vue.use(VueAxios, axios)
 
 new Vue({
   router,
