a62e560b69b76de562d7171a9670356f6964f069
gywgithub <qingyi_w@outlook.com>
2021-11-07 11:37:33 +0800

    feat: add line demo

--- src/components/Lines/LineChartI.vue
+++ src/components/Lines/LineChartI.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <h2>Line Chart I</h2>
+    <!-- <div id=""></div> -->
+    <svg width="600" height="600"></svg>
   </div>
 </template>
 <script>
-// import * as d3 from 'd3'
+import * as d3 from 'd3'
 export default {
   data () {
     return {}
   },
   mounted () {
-    // console.log(d3)
+    console.log('---')
+    console.log(d3)
+    const svg = d3.select('svg')
+    console.log(svg)
   }
 }
 </script>
