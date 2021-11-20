2a0a8f6846bc01f0a1efd0f0c4be3830bca8ec33
gywgithub <qingyi_w@outlook.com>
2021-11-20 20:08:36 +0800

    chore: add LineChartII.vue

---
+++ src/components/Lines/LineChartII.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Line Chart I</h2>
+    <svg width="800" height="500"></svg>
+  </div>
+</template>
+<script>
+import * as d3 from 'd3'
+import { howto, altplot } from '@d3/example-components'
+export default {
+  data () {
+    return {}
+  },
+  async mounted () {
+    // https://observablehq.com/@d3/line-chart
+    console.log('mounted')
+    console.log(d3)
+    console.log(howto)
+    console.log(altplot)
+  }
+}
+</script>
+<style scoped></style>
